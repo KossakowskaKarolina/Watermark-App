e3cdf50b10d0b2a3faf7d8fb22e3ad4fe4b475c3
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-02-08 13:37:32 +0100

    Catch errors

--- app.js
+++ app.js
@@ -3,32 +3,45 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
-  const image = await Jimp.read(inputFile); // metoda read służy do ładowania plików graficznych; await gwarantuje, że kompilacja nie pójdzie do przodu, dopóki plik nie zostanie załadowany
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK); // odwołanie do czcionki (Open Sans jest dostępny z paczką, ale możemy załadować własne fonty); loadFont ładuje font
-  const textData = {
-    text,
-    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-    alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-  };
-  image.print(font, 0, 0, textData, image.getWidth(), image.getHeight()); // dodajemy napis - pierwszy argument mówi, z jakiej czcionki korzystamy; drugi i trzeci decydują o umiejscowieniu tekstu; czwarty podaje treść; piąty i szósty szerokość i wysokość obrazka
-  await image.quality(100).writeAsync(outputFile); // zapisujemy obrazek jako nowy plik, wartość w quality odpowiada 100%
-  process.stdout.write('Hooray! Watermark was added!');
-  startApp();
+  try {
+    const image = await Jimp.read(inputFile); // metoda read służy do ładowania plików graficznych; await gwarantuje, że kompilacja nie pójdzie do przodu, dopóki plik nie zostanie załadowany
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK); // odwołanie do czcionki (Open Sans jest dostępny z paczką, ale możemy załadować własne fonty); loadFont ładuje font
+    const textData = {
+      text,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+    };
+
+    image.print(font, 0, 0, textData, image.getWidth(), image.getHeight()); // dodajemy napis - pierwszy argument mówi, z jakiej czcionki korzystamy; drugi i trzeci decydują o umiejscowieniu tekstu; czwarty podaje treść; piąty i szósty szerokość i wysokość obrazka
+    await image.quality(100).writeAsync(outputFile); // zapisujemy obrazek jako nowy plik, wartość w quality odpowiada 100%
+
+    process.stdout.write('Hooray! Watermark was added!');
+    startApp();
+  }
+  catch(error){
+    console.log('Something went wrong... Try again!');
+  }
 };
 
 const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) { // podobnie jw. ale trzeci parametr otrzymuje ścieżkę do pliku znaku wodnego
-  const image = await Jimp.read(inputFile);
-  const watermark = await Jimp.read(watermarkFile);
-  const x = image.getWidth() / 2 - watermark.getWidth() / 2;
-  const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+  try {
+    const image = await Jimp.read(inputFile);
+    const watermark = await Jimp.read(watermarkFile);
+    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
 
-  image.composite(watermark, x, y, { // composite służy do łączenia dwóch obrazków ze sobą
-    mode: Jimp.BLEND_SOURCE_OVER, // sposób połączenia (watermark na wierzchu)
-    opacitySource: 0.5,
-  });
-  await image.quality(100).writeAsync(outputFile);
-  process.stdout.write('Hooray! Watermark was added!');
-  startApp();
+    image.composite(watermark, x, y, { // composite służy do łączenia dwóch obrazków ze sobą
+      mode: Jimp.BLEND_SOURCE_OVER, // sposób połączenia (watermark na wierzchu)
+      opacitySource: 0.5,
+    });
+    await image.quality(100).writeAsync(outputFile);
+
+    process.stdout.write('Hooray! Watermark was added!');
+    startApp();
+  }
+  catch(error){
+    console.log('Something went wrong... Try again!');
+  }
 };
 
 const prepareOutputFilename = (filename) => {
